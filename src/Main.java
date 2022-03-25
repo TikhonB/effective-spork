4e400b77861e46ddb251e499bf28850a11afbd62
TikhonB <tishka.bogdanov@gmail.com>
2022-03-25 21:29:03 +0300

    Add StreamMain class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
     }
 }
